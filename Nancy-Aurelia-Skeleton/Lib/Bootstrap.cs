4b09d864f457e7bbd5a9010228dd88e10c69fcec
Mikael Porttila <mikael.porttila@gmail.com>
2016-02-10 22:02:22 +0100

    First setup. Created a new Nancy (Owin hosted) project and added the Aurelia TypeScript Skeleton.

---
+++ Nancy-Aurelia-Skeleton/Lib/Bootstrap.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using Owin;
+namespace NancyAureliaSkeleton {
+
+    public class Startup {
+        public void Configuration(IAppBuilder builder) {
+
+            // Tell owin to delegate requests 
+            // to nancys handler
+            builder.UseNancy();
+        }
+    }
+
+    public class ApplicationBootstrap : DefaultNancyBootstrapper {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines) {
+
+            // add some security.
+            Nancy.Security.Csrf.Enable(pipelines);
+            
+            // Set ContentType after request been handled.  
+            pipelines.AfterRequest += ctx => {
+                ctx.Response.ContentType += "; charset=utf-8";
+            };
+        }
+        
+    }
+}
---
+++ Nancy-Aurelia-Skeleton/Modules/IndexModule.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+
+namespace NancyAureliaSkeleton.Modules {
+    public class IndexModule : NancyModule {
+        public IndexModule() {
+            Get["/"] = parameters => {
+                return View["Views/index.html"];
+            };
+        }
+    }
+}
